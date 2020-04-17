adcd08317b5e2cae69ed36ce968afbc0f1351177
phas0r <burgard.jo@googlemail.com>
2020-04-17 17:05:15 +0200

    Fix problems with transitions
    This fixes the problems with transitions. It properly destroys the component when a new page is loaded.

--- src/App.svelte
+++ src/App.svelte
@@ -6,20 +6,27 @@
     initialPage,
     resolveComponent,
     transformProps = props => props
+  
+  let currentComponent
 
   Inertia.init({
     initialPage,
     resolveComponent,
     updatePage: (component, props, { preserveState }) => {
-      store.update(page => ({
-        component,
-        key: preserveState ? page.key : Date.now(),
-        props: transformProps(props),
-      }))
+      store.update(page => {
+        if (page.component !== component && currentComponent) {
+          currentComponent.$destroy()
+        }
+        return {
+          component,
+          key: preserveState ? page.key : Date.now(),
+          props: transformProps(props),
+        }
+      })
     },
   })
 </script>
 
 {#each [$store.key] as key (key)}
-<svelte:component this={$store.component} {...$store.props} />
+<svelte:component this={$store.component} bind:this={currentComponent} {...$store.props} />
 {/each}
